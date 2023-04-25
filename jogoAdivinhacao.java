e0df9e47625c9d6a320ba6014d364fe47ca70c8f
Luizhondo <120063146+LuizHondo@users.noreply.github.com>
2023-04-24 22:14:42 -0300

    Envio
    Envio de arquivo com jogo pronto

---
+++ jogoAdivinhacao.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class jogoAdivinhacao {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        int sortido = new Random().nextInt(100);
+        int tentativas = 10;
+        System.out.println("Este é o jogo de adivinhação. O objetivo é adivinhar o número sortido entre 0 e 100. Digite um número para começar");
+
+        while(tentativas > 0){
+            int chute = leitura.nextInt();
+
+            if (sortido > chute){
+                tentativas--;
+                System.out.println("O número sorteado é maior");
+            }
+            if (sortido < chute){
+                tentativas--;
+                System.out.println("O número sorteado é menor");
+                System.out.println("Você ainda possui " + tentativas + " tentativas");
+            }
+            if (sortido == chute) {
+                System.out.println("Parabéns! Você acertou!!");
+                break;
+            }
+
+        }
+        if (tentativas == 0){
+            System.out.println("Você não possui mais tentativas, o número sortido era: "+sortido+".");
+        }
+
+
+    }
+}
